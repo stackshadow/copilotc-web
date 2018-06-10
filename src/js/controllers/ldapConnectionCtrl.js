e84e5c096ce9af71de20262bcea6416c25c1308a
stackshadow <stackshadow@evilbrain.de>
2018-06-10 22:40:07 +0200

    LDAP: Add user-edit dialog

--- src/js/controllers/ldapConnectionCtrl.js
+++ src/js/controllers/ldapConnectionCtrl.js
@@ -1,33 +1,87 @@
 //main.js
 
 angular
-.module('app')
+.module('app', ['ui.grid'] )
 .controller('ldapConnectionCtrl', ldapConnectionCtrl);
 
 
-ldapConnectionCtrl.$inject = ['$scope'];
-function ldapConnectionCtrl($scope) {
+ldapConnectionCtrl.$inject = ['$scope','uiGridConstants'];
+function ldapConnectionCtrl($scope,uiGridConstants) {
 
     var connected = false;
     $scope.config = {};
     $scope.configOrig = {};
+    $scope.conButtonDisplayName = _i['connect'];
+    $scope.conButtonStyle = "btn btn-sm btn-primary";
+    $scope.user = {};
+    $scope.user.change = false;
+    $scope.user.create = false;
 
-// functions
+// language string
+    $scope._i = function( message ){
+        if( _i[message] !== undefined ){
+            return _i[message];
+        } else {
+            console.log( "Need translation: '" + message + "'" );
+            return message;
+        }
+    }
+
+
+// lists
+    var ldapUsersArray = [];
+    var ldapUsersObject = {};
+
+
+
+    $scope.gridOpts = {
+        enableSorting: true,
+        paginationPageSizes: [7, 25, 50, 100],
+        paginationPageSize: 7,
+        enableFiltering: true,
+        appScopeProvider: this,
+        columnDefs: [
+            { name: 'actions', width: 100, enableFiltering: false, cellTemplate: ' \
+            <div class="btn-group"> \
+                <button type="button" class="btn btn-success" uid="{{row.entity.uid}}"  onclick="getScope(\'ldapConnectionCtrl\').ldapUserEdit(this)"  > \
+                    <i class="fa fa-pencil" aria-hidden="true"></i> \
+                </button> \
+            </div> \
+            ' },
+            { name: 'uid',  displayName: 'ID', width: 250, sort: { direction: uiGridConstants.ASC, priority: 0 } },
+            { name: 'dn',   displayName: 'DN', minWidth: 400 },
+            { name: 'sn',   displayName: 'Vorname' },
+            { name: 'cn',   displayName: 'Name' },
+            { name: 'mail',  displayName: 'E-Mail' },
+        ],
+        data: ldapUsersArray,
+        rowHeight: 40
+    };
+
+
+// ################### config ###################
+
     $scope.refreshConState = function(){
         if( connected == false ){
-            $scope.conButtonDisplayName = "Connect";
+            $scope.conButtonDisplayName = _i['connect'];
             $scope.conButtonStyle = "btn btn-sm btn-primary";
         } else {
-            $scope.conButtonDisplayName = "Connect";
+            $scope.conButtonDisplayName = _i['disconnect'];
             $scope.conButtonStyle = "btn btn-sm btn-outline-danger";
         }
     
     }
     
     $scope.getConConfig = function(){
-        $('#ldapConnectionSettings').modal({
+        
+        $('#ldapConnectionSettings')
+        .modal({
             keyboard: false
         })
+        .on('show.bs.modal', function (event) {
+            ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'configGet', '' );
+        })
+        
          ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'configGet', '' );
     }
     
@@ -51,10 +105,101 @@ function ldapConnectionCtrl($scope) {
     }
 
     $scope.connect = function(){
-        ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'connect', '' );
+        
+        if( connected == false ){
+            ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'connect', '' );
+            return;
+        }
+        
+        if( connected == true ){
+            ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'disconnect', '' );
+            return;
+        }
+        
+    }
+    
+
+    
+
+// ################### Users ###################
+    
+    $scope.ldapUserListRefresh = function(){
+        ldapUsersObject = {};
+        ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'userlist', '' );
     }
 
+    $scope.ldapUserAdd = function(){
+        $scope.user.create = true;
+        $scope.user.change = false;
+        
+        $('#ldapUserSettings')
+        .modal({
+            keyboard: false
+        })
+        .on('show.bs.modal', function (event) {
+        })
+    }
+    
+    $scope.ldapUserEdit = function( htmlElement ){
+        $scope.user.create = false;
+        $scope.user.change = true;
 
+    // get
+        var uid = htmlElement.getAttribute( 'uid' );
+        ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'userGet', uid );
+        
+        $('#ldapUserSettings')
+        .modal({
+            keyboard: false 
+        })
+        .on('show.bs.modal', function (event) {
+            
+        })
+        
+    }
+
+    $scope.ldapUserDelete = function(){
+        $scope.user.create = false;
+        $scope.user.change = false;
+        
+        $('#ldapUserSettings').modal();
+    }
+    
+    $scope.ldapUserDeleteApply = function( uid ){
+        
+        var newUser = {};
+        newUser.action = 3;
+        newUser.uid = $scope.user.uid;
+        
+        ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'userMod', newUser );
+        
+        $('#ldapUserSettings').modal('hide');
+    }
+
+    $scope.ldapUserSave = function( uid ){
+        
+        if(  $scope.user.create == true ){
+            var newUser = {};
+            newUser.action = 1;
+            newUser.uid = $scope.user.uid;
+            newUser.mail = $scope.user.mail;
+            newUser.pw = $scope.user.pw;
+            
+            ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'userMod', newUser );
+        }
+        
+        if(  $scope.user.change == true ){
+            var newUser = {};
+            newUser.action = 2;
+            newUser.uid = $scope.user.uid;
+            newUser.mail = $scope.user.mail;
+            newUser.pw = $scope.user.pw;
+            
+            ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'userMod', newUser );
+        }
+        
+        $('#ldapUserSettings').modal('hide');
+    }
 
 
 // events
@@ -67,11 +212,14 @@ function ldapConnectionCtrl($scope) {
         if( jsonMessage.c == "state" ){
             
             if( jsonMessage.v == "connected" ){
+                logit( logitSuccess, "LDAP " + _i['connected'] );
                 connected = true;
             } else {
+                logit( logitError, "LDAP " + _i['disconnected'] );
                 connected = false;
             }
 
+            $scope.refreshConState();
             $scope.$apply();
         }
         
@@ -93,6 +241,49 @@ function ldapConnectionCtrl($scope) {
         if( jsonMessage.c == "configSaved" ){
             $('#ldapConnectionSettings').modal('hide');
         }
+
+
+        if( jsonMessage.c == "users" ){
+            var newUserList = JSON.parse( jsonMessage.v );
+            
+            
+        // merge userlist
+            $.extend( ldapUsersObject, newUserList );
+            
+            ldapUsersArray = [];
+            for( ldapUserDN in ldapUsersObject ){
+                ldapUsersArray.push( ldapUsersObject[ldapUserDN] );
+            }
+            
+            $scope.gridOpts.data = ldapUsersArray;
+
+        // update view
+            $scope.$apply();
+        }
+
+
+        if( jsonMessage.c == "user" ){
+            var jsonUser = JSON.parse( jsonMessage.v );
+            if( jsonUser === null ) return;
+            for( jsonKey in jsonUser ){
+                $scope.user = jsonUser[jsonKey];
+            }
+            
+            $scope.$apply();
+            return;
+        }
+
+        if( jsonMessage.c == "userAdded" ){
+            $scope.ldapUserListRefresh();
+        }
+        if( jsonMessage.c == "userChanged" ){
+            $scope.ldapUserListRefresh();
+        }
+        if( jsonMessage.c == "userDeleted" ){
+            $scope.ldapUserListRefresh();
+        }
+        
+
     }
 
 // register websocket events
@@ -103,14 +294,16 @@ function ldapConnectionCtrl($scope) {
         ws.off( 'onJsonMessage', $scope.onJsonMessage );
     });
 
+
+
+
+
+
 // show button state
     $scope.refreshConState();
 
 // initial request the connection state of ldap
     ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'stateGet', '' );
-
 }
 
-$('#ldapConnectionSettings').on('show.bs.modal', function (event) {
-    ws.sendMsg( genUUID(), 'wsclient', myNodeName, 'ldap', 'configGet', '' );
-})
+
