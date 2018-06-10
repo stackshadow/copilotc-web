245e487aeedd4b5f8004d65b03e193b64d039aac
stackshadow <stackshadow@evilbrain.de>
2018-06-10 22:38:50 +0200

    Add i18n support

--- src/i18n/de_DE.js
+++ src/i18n/de_DE.js
@@ -7,4 +7,19 @@ _i['disconnect'] = "Trennen";
 _i['disconnected'] = "Getrennt";
 
 _i['setup'] = "Einstellungen";
+_i['Connection settings'] = "Verbindungseinstellung";
 
+_i['user'] = "Benutzer";
+_i['Close'] = "Schließen";
+_i['Save Changes'] = "Änderung speichern";
+_i['Delete'] = "Löschen";
+
+// ldap
+_i['LDAP-Connection-String'] = "Einstellungen";
+_i['Admin-DN'] = "Admin-DN";
+_i['Admin-PW'] = "Admin-PW";
+_i['Login-DN'] = "Login-DN";
+_i['Login-PW'] = "Login-PW";
+_i['Base-DN'] = "Base-DN";
+_i['Group-DN'] = "Group-DN";
+_i['User-DN'] = "User-DN";
